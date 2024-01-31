148af64dec5d3b898648418dd4d06c4105a8716e
Snowball <108003920+Snowball-0@users.noreply.github.com>
2024-01-31 12:18:20 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -18,6 +18,8 @@ class Database:
             caption=None,
             prefix=None,
             suffix=None,
+            metadata=False,
+            metadata_code=""" -map 0 -c:s copy -c:a copy -c:v copy -metadata title="Powered By:- @Kdramaland" -metadata author="@Snowball_Official" -metadata:s:s title="Subtitled By :- @Kdramaland" -metadata:s:a title="By :- @Kdramaland" -metadata:s:v title="By:- @Snowball_Official" """
         )
 
     async def add_user(self, b, m):

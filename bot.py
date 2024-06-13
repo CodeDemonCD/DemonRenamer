c5f7cefc17b31067a1551b54877d13a5382a1cca
CФDΞ DΞMФИ <158114979+CodeDemonCD@users.noreply.github.com>
2024-06-14 00:39:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ class Bot(Client):
 
     def __init__(self):
         super().__init__(
-            name="SnowRenamer",
+            name="DemonRenamer",
             api_id=Config.API_ID,
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
